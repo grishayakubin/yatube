c003d4e74153a6e3762d09412522fbc29a7ca26b
Grisha Yakubin <g.yakubin03@gmail.com>
2023-01-09 20:23:47 +0300

    created templates, connected boostrap

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,9 +3,13 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Главная страница')
+    text = 'Это главная страница проекта Yatube'
+    template = 'posts/index.html'
+    context = {'text' : text}
+    return render(request, template, context)
 
 def group_posts(request, slug):
-    return HttpResponse(f'Список постов {slug}')
-
-
+    text = 'Здесь будет информация о группах проекта Yatube'
+    template = 'posts/group_list.html'
+    context = {'text' : text,}
+    return render(request, template, context, slug)
--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -51,11 +51,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'yatube.urls'
-
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,4 +118,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = (os.path.join(BASE_DIR, '/static/'))
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
